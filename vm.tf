9312a6e9af38ecd8425625775b831d978093ec99
Pivopil <myworkplace8@gmail.com>
2020-12-23 22:10:37 +0200

    Added vm

---
+++ vm.tf
@@ -0,0 +1,18 @@
+data "google_compute_zones" "available" {}
+
+resource "google_compute_instance" "default" {
+  project = local.project_name
+  zone = data.google_compute_zones.available.names[0]
+  name = "tf-compute-1"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    subnetwork = module.vpc.subnets_names[0]
+    access_config {
+    }
+  }
+}
