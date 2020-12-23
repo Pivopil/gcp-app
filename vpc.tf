22124b32a8fc20044fdd897a6f711984dc635f33
Pivopil <myworkplace8@gmail.com>
2020-12-23 19:47:55 +0200

    Added APIs to the project

--- vpc.tf
+++ vpc.tf
@@ -1,26 +1,26 @@
-locals {
-  subnet_01 = "${var.network_name}-subnet-01"
-  subnet_02 = "${var.network_name}-subnet-02"
-  subnet_03 = "${var.network_name}-subnet-03"
-}
-
-module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "~> 2.6"
-  project_id   = google_project.project.project_id
-  network_name = var.network_name
-
-  subnets = [
-    {
-      subnet_name   = local.subnet_01
-      subnet_ip     = "10.10.10.0/24"
-      subnet_region = var.region
-    },
-    {
-      subnet_name           = local.subnet_02
-      subnet_ip             = "10.10.20.0/24"
-      subnet_region         = var.region
-      subnet_private_access = "true"
-    }
-  ]
-}
+//locals {
+//  subnet_01 = "${var.network_name}-subnet-01"
+//  subnet_02 = "${var.network_name}-subnet-02"
+//  subnet_03 = "${var.network_name}-subnet-03"
+//}
+//
+//module "vpc" {
+//  source  = "terraform-google-modules/network/google"
+//  version = "~> 2.6"
+//  project_id   = google_project.project.project_id
+//  network_name = var.network_name
+//
+//  subnets = [
+//    {
+//      subnet_name   = local.subnet_01
+//      subnet_ip     = "10.10.10.0/24"
+//      subnet_region = var.region
+//    },
+//    {
+//      subnet_name           = local.subnet_02
+//      subnet_ip             = "10.10.20.0/24"
+//      subnet_region         = var.region
+//      subnet_private_access = "true"
+//    }
+//  ]
+//}
