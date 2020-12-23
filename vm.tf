55f1ecc90034410298aa113b05ddd5ef0c41e892
Pivopil <myworkplace8@gmail.com>
2020-12-23 22:23:48 +0200

    Updated vm configs

--- vm.tf
+++ vm.tf
@@ -1,18 +1,16 @@
-data "google_compute_zones" "available" {}
-
-//resource "google_compute_instance" "default" {
-//  project      = google_project.project.project_id
-//  zone         = data.google_compute_zones.available.names[0]
-//  name         = "tf-compute-1"
-//  machine_type = "f1-micro"
-//  boot_disk {
-//    initialize_params {
-//      image = "debian-cloud/debian-9"
-//    }
-//  }
-//  network_interface {
-//    network = local.subnet_01
-//    access_config {
-//    }
-//  }
-//}
+resource "google_compute_instance" "default" {
+  project      = google_project.project.project_id
+  zone         = "${var.region}-a"
+  name         = "tf-compute-1"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = local.subnet_01
+    access_config {
+    }
+  }
+}
